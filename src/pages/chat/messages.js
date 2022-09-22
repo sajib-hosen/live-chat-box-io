3c820cf9f16905aab4b2c8bf06291e6c993bf738
sajib-hosen <sajib.201h@gmail.com>
2022-09-22 16:00:12 +0600

    working on live chat

--- src/pages/chat/messages.js
+++ src/pages/chat/messages.js
@@ -4,12 +4,15 @@ import styles from './styles.module.css';
 import { useState, useEffect, useRef } from 'react';
 
 const Messages = ({ socket }) => {
-  const [messagesRecieved, setMessagesReceived] = useState([]);
 
+  const [messagesRecieved, setMessagesReceived] = useState([]);
   const messagesColumnRef = useRef(null); // Add this
 
-  // Runs whenever a socket event is recieved from the server
-  useEffect(() => {
+
+
+
+
+  useEffect(() => {    // Runs whenever a socket event is recieved from the server
     socket.on('receive_message', (data) => {
       console.log(data);
       setMessagesReceived((state) => [
@@ -22,46 +25,57 @@ const Messages = ({ socket }) => {
       ]);
     });
 
-    // Remove event listener on component unmount
-    return () => socket.off('receive_message');
+    
+    return () => socket.off('receive_message'); // Remove event listener on component unmount
   }, [socket]);
 
-  // Add this
-  useEffect(() => {
-    // Last 100 messages sent in the chat room (fetched from the db in backend)
+
+
+
+
+  useEffect(() => { // Last 100 messages sent in the chat room (fetched from the db in backend)
+    
     socket.on('last_100_messages', (last100Messages) => {
       console.log('Last 100 messages:', JSON.parse(last100Messages));
       last100Messages = JSON.parse(last100Messages);
-      // Sort these messages by __createdtime__
-      last100Messages = sortMessagesByDate(last100Messages);
+      last100Messages = sortMessagesByDate(last100Messages);  // Sort these messages by __createdtime__
       setMessagesReceived((state) => [...last100Messages, ...state]);
     });
 
     return () => socket.off('last_100_messages');
   }, [socket]);
 
-  // Add this
-  // Scroll to the most recent message
-  useEffect(() => {
+
+
+
+
+
+
+  useEffect(() => {   // Scroll to the most recent message
     messagesColumnRef.current.scrollTop =
       messagesColumnRef.current.scrollHeight;
   }, [messagesRecieved]);
 
-  // Add this
+
   function sortMessagesByDate(messages) {
     return messages.sort(
       (a, b) => parseInt(a.__createdtime__) - parseInt(b.__createdtime__)
     );
   }
 
-  // dd/mm/yyyy, hh:mm:ss
-  function formatDateFromTimestamp(timestamp) {
+
+
+
+
+  function formatDateFromTimestamp(timestamp) {   // dd/mm/yyyy, hh:mm:ss
     const date = new Date(timestamp);
     return date.toLocaleString();
   }
 
+
+
+
   return (
-    // Add ref to this div
     <div className={styles.messagesColumn} ref={messagesColumnRef}>
       {messagesRecieved.map((msg, i) => (
         <div className={styles.message} key={i}>
